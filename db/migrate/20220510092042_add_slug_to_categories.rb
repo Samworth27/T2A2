5901be812824b94279bad690446f1eb250cc6b05
Sam Mitchell <samworth27@gmail.com>
2022-05-12 20:29:01 +1000

    fixed typo in migration from the great category change

--- db/migrate/20220510092042_add_slug_to_categories.rb
+++ db/migrate/20220510092042_add_slug_to_categories.rb
@@ -1,4 +1,4 @@
-class AddSlugTocategories < ActiveRecord::Migration[7.0]
+class AddSlugToCategories < ActiveRecord::Migration[7.0]
   def change
     add_column :categories, :slug, :string
     add_index :categories, :slug, unique: true
