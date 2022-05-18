13a95027d4f79540a0159d3e29644283e5a2a8ab
Sam Mitchell <samworth27@gmail.com>
2022-05-18 14:14:19 +1000

    listings complete

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 class ListingsController < ApplicationController
-
   def index; end
 
-  def show; end
-
+  def show
+    @listing = Listing.find(params[:id])
+    @item = @listing.item
+    @category = @item.category
+    @measurement = @listing.quantity > 1 ? @listing.measurement.plural : @listing.measurement.unit
+    if @listing.measurement.measurement_for == 'count'
+      @name = @listing.quantity > 1 ? @item.plural : @item.name
+    else
+      @name = @item.plural
+    end
+  end
 end
